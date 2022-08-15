36f6b71e0ff2fb7d55f8ca250d945547cb5ed554
arjreddy <105601239+arjreddy@users.noreply.github.com>
2022-08-15 20:27:00 +0530

    Update 1.py

--- chapter-12/1.py
+++ chapter-12/1.py
@@ -1,1 +1,12 @@
+def printRange(num):
+    if num==0:
+        return;
+    printRange(num-1)
+    print(num)
 
+def main():
+    num=int(input('Enter a number: '))
+    printRange(num)
+    
+if __name__=='__main__':
+    main()
