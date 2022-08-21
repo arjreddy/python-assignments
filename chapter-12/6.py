746f19d0e8012d097a9ce95d44895633ebb1e3b5
arjreddy <105601239+arjreddy@users.noreply.github.com>
2022-08-21 11:13:34 +0530

    Update 6.py

--- chapter-12/6.py
+++ chapter-12/6.py
@@ -1,10 +1,12 @@
+# Calculates sum of first n natural numbers and returns it.
 def getSum(n):
     if n<=0:
         return 0
     return n + getSum(n-1)
 
 def main():
-    n = int(input('Enter n: '))
+    n = int(input('Enter an integer number: '))
+    # call function to find sum of first n natural numbers.
     sum = getSum(n)
     print(f'Sum of first {n} natural numbers is {sum}')
 
