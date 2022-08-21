2dfc2a1f4b78f279d790596c716a49c38ecbe3d3
arjreddy <105601239+arjreddy@users.noreply.github.com>
2022-08-21 10:57:39 +0530

    Update 2.py

--- chapter-12/2.py
+++ chapter-12/2.py
@@ -1,13 +1,15 @@
+# Returns multiplication of two integers.
 def multiplication(x, y):
     if y==0:
         return 0
     return x + multiplication(x, y-1)
 
 def main():
-    x=int(input("Enter x : "))
-    y=int(input("Enter y : "))
+    x=int(input("Enter first integer number : "))
+    y=int(input("Enter second integer number : "))
+    # Call to multiplication function.
     multi = multiplication(x, y)
-    print(f'{x} * {y} = {multi}')
+    print(f'Multiplication of two numbers is: {x} * {y} = {multi}')
 
 if __name__=='__main__':
     main()
