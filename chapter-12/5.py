d8d86a4f016408ba426cec7db4f620a7868d73d8
arjreddy <105601239+arjreddy@users.noreply.github.com>
2022-08-21 11:10:39 +0530

    Update 5.py

--- chapter-12/5.py
+++ chapter-12/5.py
@@ -1,4 +1,4 @@
-
+# calculates sum of all the elements in a list recursively and returns it.
 def findSumRec(lst, n):
     if n==0:
         return 0
@@ -6,11 +6,13 @@ def findSumRec(lst, n):
 
 def main():
     lst=[]
-    n=int(input("Enter n: "))
+    n=int(input("Enter a number: "))
+     # prompt for n inputs.
+    print("We will prompt you enter ",n, " numbers.")
     for x in range(n):
-        lst.append(int(input()))
+        lst.append(int(input("Enter a Number : ")))
     sum = findSumRec(lst, len(lst))
-    print(f'sum of {lst} is {sum}')
+    print(f'sum of all the elements of {lst} is {sum}')
 
 if __name__=='__main__':
     main()
