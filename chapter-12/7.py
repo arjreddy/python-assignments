ef7f5a373219044c763005e3647da1a6ad06aeb0
arjreddy <105601239+arjreddy@users.noreply.github.com>
2022-08-21 11:18:46 +0530

    Update 7.py

--- chapter-12/7.py
+++ chapter-12/7.py
@@ -1,11 +1,12 @@
+# calculates n power e and returns it.
 def power(n, e):
     if e==0:
         return 1
     return n * power(n, e-1)
 
 def main():
-    n = int(input("enter n: "))
-    pow = int(input("enter pow: "))
+    n = int(input("enter a number: "))
+    pow = int(input("enter an exponent: "))
     ans = power(n, pow)
     print(f'{n} ^ {pow} = {ans}')
 
