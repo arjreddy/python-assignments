7596bebfb16bdf2e2c631216271a28a595d446c6
arjreddy <105601239+arjreddy@users.noreply.github.com>
2022-08-21 11:35:58 +0530

    Update 8.py

--- chapter-12/8.py
+++ chapter-12/8.py
@@ -1,4 +1,7 @@
+# The ackermann function.
 def ackermann(m,n):
+    if (m<0) or (n<0):
+        raise Exception('Exceeds recursion limit')
     if m == 0:
         return n + 1
     if n == 0:
@@ -6,10 +9,16 @@ def ackermann(m,n):
     return ackermann(m - 1, ackermann(m, n - 1))
 
 def main():
-    n = int(input("Enter n: "))
-    m = int(input("Enter m: "))
-    ans = ackermann(m,n)
-    print(f'ackermann({m},{n}) = {ans}')
+    print("ackermann function")
+    n = int(input("Enter first number: "))
+    m = int(input("Enter second number: "))
+    try:
+
+       ans = ackermann(m,n)
+       print(f'ackermann({m},{n}) = {ans}')
+    except Exception as error:
+        print("Error occurred: ", error.args)
+      
 
 if __name__=='__main__':
     main()
